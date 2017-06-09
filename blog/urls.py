9fcfce8f36214e4e5756a2c219af95cce8f1d45e
kellihicks <kelli_hicks1@hotmail.com>
2017-06-09 03:39:25 +0000

    all done

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+   url(r'^$', views.post_list, name='post_list'),url(r'^json_all_posts$', views.json_all_posts, name='json_all_posts'),
+ url(r'^spa_post_list$', views.spa_post_list, name='spa_post_list'),
 ]
+ 
+   
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.http import JsonResponse
+from django.core import serializers
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def spa_post_list(request):
+    return render(request, 'blog/spa_post_list.html', {})
+
+def json_all_posts(request):
+    posts = Post.objects.order_by('published_date')
+    data = serializers.serialize('json', posts)
+    return JsonResponse(data, safe=False)
+
+
 
